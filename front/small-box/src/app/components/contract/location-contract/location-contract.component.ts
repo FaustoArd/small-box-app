91486d040d428813ed923d125324533b66d051bc
Fausto Arduino <faustard@gmail.com>
2024-02-25 15:50:25 -0300

    .

--- front/small-box/src/app/components/contract/location-contract/location-contract.component.ts
+++ front/small-box/src/app/components/contract/location-contract/location-contract.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { LocationContractDto } from 'src/app/models/locationContractDto';
 import { LocationContractService } from 'src/app/services/location-contract.service';
 import { SnackBarService } from 'src/app/services/snack-bar.service';
@@ -17,7 +17,45 @@ locationContract!:LocationContractDto;
     private formBuilder:FormBuilder){}
 
     locationContractFormBuilder = this.formBuilder.group({
-      
-    })
+      homeDependency:['',Validators.required],
+      decree:['',Validators.required],
+      contractedName:['', Validators.required],
+      contractedCountry:['', Validators.required],
+      contractedDni:['',Validators.required],
+      contractedAfipRank:['', Validators.required],
+      contractedCuit:['',Validators.required],
+      contractedAddress:['',Validators.required],
+      contractedAddressFloor:['',Validators.required],
+      contractedAddressDptoNumber:['',Validators.required],
+      contractedCity:['',Validators.required],
+      contractedCounty:['',Validators.required],
+      contractedLegalAddress:['',Validators.required],
+      contractedTask:['',Validators.required],
+      vigencyPeriodStart:['',Validators.required],
+      vigencyPeriodEnd:['',Validators.required],
+      training:['',Validators.required],
+      trainingHour:['',Validators.required],
+      contractedPrice:['',Validators.required],
+      contractedDate:['',Validators.required]
+
+    });
+
+    get homeDependency(){
+      return this.locationContractFormBuilder.controls.homeDependency;
+    }
+
+    get decree(){
+      return this.locationContractFormBuilder.controls.decree;
+    }
+    get contractedName(){
+      return this.locationContractFormBuilder.controls.contractedName;
+    }
+
+    get contractedCountry(){
+      return this.locationContractFormBuilder.controls.contractedCountry;
+    }
+    get contractedDni(){
+      return this.locationContractFormBuilder.controls.contractedDni;
+    }
 
 }
