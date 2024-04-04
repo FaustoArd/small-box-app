c10a36f3ea73f7df8f22a6f6c98337a6101a8f78
Fausto Arduino <faustard@gmail.com>
2024-04-04 20:28:41 -0300

    .

--- front/small-box/src/app/components/navbar/navbar.component.ts
+++ front/small-box/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieStorageService } from 'src/app/services/cookie-storage.service';
+import { OrganizationService } from 'src/app/services/organization.service';
 
 
 @Component({
@@ -10,18 +11,20 @@ import { CookieStorageService } from 'src/app/services/cookie-storage.service';
 })
 export class NavbarComponent implements OnInit {
 
-
+userAuth!:boolean;
 adminAuth!:boolean;
 superUserAuth!:boolean;
 currentUsername!:string;
 destinationsList!:Array<string>
 
-constructor(private cookieService:CookieStorageService,private router:Router){}
+constructor(private cookieService:CookieStorageService,private router:Router,private organizationService:OrganizationService){}
 
 
 ngOnInit(): void {
-    this.adminAuth = this.decodeAdminToken();
+  this.decodeToken();
+    /*this.adminAuth = this.decodeAdminToken();
     this.superUserAuth = this.decodeSuperUserToken();
+    this.userAuth = this.decodeUserToken();*/
     this.currentUsername = this.cookieService.getCurrentUsername();
     
 }
@@ -29,8 +32,20 @@ onLogout(){
     this.cookieService.deleteToken();
     this.router.navigateByUrl("/login")
   }
+
+  decodeToken():boolean{
+    let token = this.cookieService.getToken();
+    let decodedJWT = JSON.parse(window.atob(token.split('.')[1]));
+    if(decodedJWT.roles==='ADMIN'){
+      return this.adminAuth = true;
+    }else if(decodedJWT.roles==='SUPERUSER'){
+      return this.superUserAuth= true;
+    }else{
+      return this.userAuth = true;
+    }
+  }
   
-  decodeAdminToken():boolean{
+ /* decodeAdminToken():boolean{
     let token = this.cookieService.getToken();
     let decodedJWT = JSON.parse(window.atob(token.split('.')[1]));
     if(decodedJWT.roles==='ADMIN'){
@@ -42,11 +57,23 @@ onLogout(){
   decodeSuperUserToken():boolean{
     let token = this.cookieService.getToken();
     let decodedJWT = JSON.parse(window.atob(token.split('.')[1]));
+
     if(decodedJWT.roles==='SUPERUSER'){
       return true;
     }
     return false;
   }
+
+  decodeUserToken():boolean{
+    let token = this.cookieService.getToken();
+    let decodedJWT = JSON.parse(window.atob(token.split('.')[1]));
+    console.log(decodedJWT.role)
+    if(decodedJWT.role==='USER'){
+      return true;
+    }else{
+      return false;
+    }
+  } */
 
 
 }
